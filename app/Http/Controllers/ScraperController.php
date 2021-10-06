37b9608f807d3b41ea62ac25dfdfbea7f2f5299b
Marco <marco.espinoza@grow-analytics.com.pe>
2021-10-06 11:31:55 -0500

    Scraping arcalauquen

--- app/Http/Controllers/ScraperController.php
+++ app/Http/Controllers/ScraperController.php
@@ -7,24 +7,31 @@ use Illuminate\Http\Request;
 class ScraperController extends Controller
 {
     public function index(Client $client){
-        $crawler = $client->request('GET', 'https://www.arcalauquen.cl/3-papeles');
-        
-        $producto =  $crawler->filter('.js-product-miniature-wrapper')->each(function($node){
-            // $imagenProducto = $node->filter("[class='thumbnail product-thumbnail']")->attr('src');
-            $nombreProducto = $node->filter("[class='h3 product-title']");
-            $precioProducto = $node->filter("[class='product-price-and-shipping']");
-            $urlProducto = $node->filter("[class='thumbnail product-thumbnail']")->extract(array('href'));
-            // echo $imagenProducto->text() . '<br>';
-            echo $nombreProducto->text() . '<br>';
-            echo $precioProducto->text() . '<br>';
-            var_dump($urlProducto);
-            echo  '<br>';
-            // dd($imagenProducto);
-            // $datosProducto = [];
-            // $datosProducto['nombre']=$nombreProducto;
-            // $datosProducto['precio']=$precioProducto;
-            // var_dump($datosProducto);
-        });
-        // dd($producto->html());
+
+        echo "FECHA : " . date("d/m/Y") . "<br>";
+        for($i=1; $i<=4; ++$i)
+        {
+            $paginaURL = "https://www.arcalauquen.cl/3-papeles?page=$i";
+            $crawler = $client->request('GET', $paginaURL);
+            $tituloCategoria = $crawler->filter("[class='h1 page-title']")->first();
+            echo $tituloCategoria->text() . '<br>';
+
+            $crawler->filter(".js-product-miniature-wrapper")->each(function($node){
+                $imagenProducto = $node->filter(".product-thumbnail > img")->attr('data-src');
+                $nombreProducto = $node->filter("[class='h3 product-title']");
+                $precioProducto = $node->filter("[class='product-price-and-shipping']");
+                $urlProducto = $node->filter("[class='thumbnail product-thumbnail']")->attr('href');
+                
+                echo 'Nombre Producto: '. $nombreProducto->text() . '<br>';
+                echo 'Precio: '. $precioProducto->text() . '<br>';
+                echo 'URL Producto: ';
+                var_dump($urlProducto);
+                echo  '<br>';
+                echo 'Imagen: ';
+                var_dump($imagenProducto);
+                echo '<br>';
+                echo '<br>';
+            });
+        }
     }
 }
